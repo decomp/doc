e587a73c897c890241c035f11005eae961a02921
mewmew <rnd0x00@gmail.com>
2015-03-22 01:31:17 +0100

    poster: Add demo with nested for loops.

---
+++ poster/demo_nested/foo.c
@@ -0,0 +1,15 @@
+int main(int argc, char **argv) {
+	int i, j, x, sum;
+
+	sum = 0;
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 7; j++) {
+			if (i < j) {
+				sum += i;
+			} else {
+				sum += j;
+			}
+		}
+	}
+	return sum % 256;
+}
