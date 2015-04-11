76987deaac5212ac59d69a57d7928f56ba89c9bc
mewmew <rnd0x00@gmail.com>
2015-04-11 16:59:48 +0200

    poster/demo_post: Add post-test loop demo.

---
+++ poster/demo_post/post_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	int i, n;
+
+	i = 0;
+	n = 1;
+	do {
+		if (i < 10) {
+			i++;
+			n *= 2;
+		} else {
+			i += 4;
+			n *= 3;
+		}
+	} while(i < 15);
+	return n%123;
+}
